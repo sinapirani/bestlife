bce37ed789a96df26968107de074f5917cb23dd8
sinapirani <realsinapirani@gmail.com>
2022-08-26 15:00:42 +0430

    form error handling ok

--- pages/auth/SnackHandler.tsx
+++ pages/auth/SnackHandler.tsx
@@ -6,19 +6,21 @@ import { SnackbarOrigin } from "@mui/material"
 
 interface stateInterface extends SnackbarOrigin {
     open: boolean,
-    message: string
+    message: string,
+    bg: string,
+    color: string
 }
 type TransitionProps = Omit<SlideProps, 'direction'>;
 const transition = (props: TransitionProps) => {
     return <Slide {...props} direction='up' />
 }
 
-const ErorrSnack = (props) => {
+const SnackHandler = (props) => {
 
     const state: stateInterface = props.state
     const setState = props.setState
 
-    const { open, vertical, horizontal, message } = state
+    const { open, vertical, horizontal, message, bg, color } = state
 
     useEffect(()=>{
         console.table([['open',open],['vertical',vertical],['horizental',horizontal],['message',message]]);
@@ -31,15 +33,15 @@ const ErorrSnack = (props) => {
             anchorOrigin={{ vertical, horizontal }}
             open={open}
             TransitionComponent={transition}
-            autoHideDuration={2000}
+            autoHideDuration={4000}
             onClose={() => setState({ ...state, open: false })}
         >
             <SnackbarContent
-                sx={{ 'background': 'red', color: 'black' }}
+                sx={{ 'background': bg, color}}
                 message={message}
             />
         </Snackbar>
     )
 }
 
-export default ErorrSnack
+export default SnackHandler
--- pages/auth/signup.tsx
+++ pages/auth/signup.tsx
@@ -1,12 +1,21 @@
 
 import { Slide, SlideProps, SnackbarOrigin } from "@mui/material";
 import React, { useState } from "react"
-import ErorrSnack from "./errorSnack";
+import SnackHandler from "./SnackHandler";
 
 
 interface stateInterface extends SnackbarOrigin {
     open: boolean,
-    message: string
+    message: string,
+    bg: string,
+    color: string
+}
+
+interface userInterface {
+    name: string,
+    username: string,
+    email: string,
+    password: string
 }
 
 const Signup = () => {
@@ -15,17 +24,39 @@ const Signup = () => {
         message: '',
         open: false,
         horizontal: 'left',
-        vertical: 'bottom'
+        vertical: 'bottom',
+        bg: '',
+        color: ''
     })
     const { open, vertical, horizontal } = state
 
-    const fireError = (text:string) => {
-        setState({ ...state, open: true, message: text })
+    const fireError = (text:string,) => {
+        setState({ ...state, open: true, message: text, bg: 'red', color: 'white' })
     }
+    const fireSuccess = (text: string,) => {
+        setState({ ...state, open: true, message: text, bg: 'green', color: 'white' })
+    }
+
+
+
+    const [user, setUser] = useState<userInterface>({
+        name: '',
+        username: '',
+        email: '',
+        password: ''
+    })
+
+    const {name, username, email, password} = user
 
     const formHandler = (e: React.FormEvent) => {
         e.preventDefault()
-        fireError('email is invalid')
+        if (username.length > 20) return fireError('یوزرنیم باید کمتر از 20 کاراکتر باشد')
+        if (!username.match(/^[a-z][a-z0-9]*$/i)) return fireError('یوزرنیم باید ترکیبی از عدد و حروف انگلیسی باشد و با عدد شروع نشود')
+        if (name.length > 30) return fireError('نام باید کوتاه تر از 30 کاراکتر باشد')
+        if (!password.match(/^(?=.*[0-9])(?=.*[!@#$%^&*])[a-zA-Z0-9!@#$%^&*]{6,16}$/)) return fireError(' پسورد باید بین 6 تا 6 کاراکتر و شامل حروف و عددانگلیسی و حروف ویژه($#%) باشد')
+
+        return fireSuccess('با موفقیت انجام شد')
+
     }
 
     const stateChanger = (value) => {
@@ -37,14 +68,14 @@ const Signup = () => {
     return(
         <div className="w-full h-screen flex justify-center items-center">
 
-            <ErorrSnack state={state} setState={stateChanger} />
+            <SnackHandler state={state} setState={stateChanger} />
 
             <div className=" py-12 px-14 bg-[#1f283a] rounded-xl flex justify-center items-center">
                 <form onSubmit={formHandler} className=" gap-y-1 flex flex-col justify-center items-center">
-                    <input className=" w-11/12 rounded-md outline-none bg-[#35435e] px-4 py-2" type="text" placeholder="نام و نام خانوادگی" />
-                    <input className=" w-11/12 rounded-md outline-none bg-[#35435e] px-4 py-2" type="email" placeholder='ایمیل' />
-                    <input className=" w-11/12 rounded-md outline-none bg-[#35435e] px-4 py-2" type="text" placeholder="یوزر نیم" />
-                    <input className=" w-11/12 rounded-md outline-none bg-[#35435e] px-4 py-2" type="password" placeholder="پسورد" />
+                    <input onChange={(e:React.ChangeEvent<HTMLInputElement>)=> setUser({...user, name: e.currentTarget.value})} className=" w-11/12 rounded-md outline-none bg-[#35435e] px-4 py-2" type="text" placeholder="نام و نام خانوادگی" />
+                    <input onChange={(e:React.ChangeEvent<HTMLInputElement>)=> setUser({...user, email: e.currentTarget.value})} className=" w-11/12 rounded-md outline-none bg-[#35435e] px-4 py-2" type="email" placeholder='ایمیل' />
+                    <input onChange={(e:React.ChangeEvent<HTMLInputElement>)=> setUser({...user, username: e.currentTarget.value})} className=" w-11/12 rounded-md outline-none bg-[#35435e] px-4 py-2" type="text" placeholder="یوزر نیم" />
+                    <input onChange={(e:React.ChangeEvent<HTMLInputElement>)=> setUser({...user, password: e.currentTarget.value})} className=" w-11/12 rounded-md outline-none bg-[#35435e] px-4 py-2" type="password" placeholder="پسورد" />
                     <input className=" w-11/12 mt-4 px-5 py-2 bg-yellow-500 rounded-lg text-gray-800" type="submit" value="ثبت نام" />
                 </form>
             </div>
