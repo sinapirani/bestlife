25c98c4ded3e48f202e9c45006c0a43a55851115
sinapirani <realsinapirani@gmail.com>
2022-08-29 14:50:16 +0430

    add link to emptyAlert btn

--- components/dashboard/emptyAlert.tsx
+++ components/dashboard/emptyAlert.tsx
@@ -1,10 +1,14 @@
+import Link from "next/link"
 
 
 const EmptyAlert = () => {
     return(
         <div className=" w-5/6 py-6 gap-y-4 flex flex-col justify-center items-center rounded-md mx-auto mt-12 bg-elementbg">
             <p>شما هیچ توانایی،منبع،مانع ای اضافه نکردید</p>
-            <button className=" bg-yellow-500 text-gray-800 px-4 py-1 rounded-md">اضافه کردن</button>
+            <Link href={'/skills'}>
+                <button className=" bg-yellow-500 hover:bg-yellow-500/70 duration-300 text-gray-800 px-4 py-1 rounded-md">اضافه کردن</button>
+            </Link>
+            
         </div>
     )
 }
